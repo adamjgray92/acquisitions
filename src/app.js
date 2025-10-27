558c324f172b69d9a0f1b4f7bcfa738139bce5ed
Adam <adamjamesgray.1992@gmail.com>
2025-10-27 23:04:09 +0000

    Add logger and middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+
+import logger from '#config/logger.js';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', {
+    stream: {
+      write: message => logger.info(message.trim()),
+    },
+  })
+);
+
 app.get('/', (req, res) => {
-  res.status(200).send('Heelllllooooooo');
+  logger.info('Hello world');
+  res.status(200).send('Hello world');
 });
 
 export default app;
